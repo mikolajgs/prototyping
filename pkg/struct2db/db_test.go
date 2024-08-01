e4e7a26a9ecaad58aba4f0b6c18a0c5683873746
Mikolaj Gasior <g@f0x.pl>
2024-08-01 12:43:14 +0200

    Remove the idea of implementing cascade deleting

--- pkg/struct2db/db_test.go
+++ pkg/struct2db/db_test.go
@@ -35,22 +35,6 @@ type TestStruct struct {
 	Key string `json:"key" 2db:"req uniq lenmin:30 lenmax:255"`
 }
 
-// Test structs for DeleteHorizontal
-type TestProduct struct {
-	ID   int64  `json:"test_product_id"`
-	Name string `json:"name"`
-}
-type TestProductPrice struct {
-	ID            int64 `json:"test_product_price_id"`
-	Price         int   `json:"price"`
-	TestProductID int64 `json:"test_product_id"`
-}
-type TestProductInfo struct {
-	ID            int64  `json:"test_product_info_id"`
-	Info          string `json:"info"`
-	TestProductID int64  `json:"test_product_id"`
-}
-
 func getTestStructWithData() *TestStruct {
 	ts := &TestStruct{}
 	ts.Flags = 4
@@ -71,15 +55,6 @@ func getTestStructWithData() *TestStruct {
 func recreateTestStructTable() {
 	testController.DropTable(&TestStruct{})
 	testController.CreateTable(&TestStruct{})
-}
-
-func recreateTestProductTables() {
-	testController.DropTable(&TestProduct{})
-	testController.CreateTable(&TestProduct{})
-	testController.DropTable(&TestProductPrice{})
-	testController.CreateTable(&TestProductPrice{})
-	testController.DropTable(&TestProductInfo{})
-	testController.CreateTable(&TestProductInfo{})
 }
 
 func areTestStructObjectsSame(ts1 *TestStruct, ts2 *TestStruct) bool {
@@ -286,35 +261,4 @@ func TestGetWithoutFilters(t *testing.T) {
 	if testStructs[2].(*TestStruct).Age != 47 {
 		t.Fatalf("Get failed to return correct list of objects, want %v, got %v", 47, testStructs[2].(*TestStruct).Age)
 	}
-}
-
-// TestDeleteHorizontal tests if DeleteHorizontal can delete structs that are usually linked with JOIN
-// in the SQL query into row:
-// SELECT ... FROM tbl1 INNER JOIN tbl2 ON tbl1.id=tbl2.tbl1_id INNER JOIN tlb3 tbl1.id=tbl3.tbl1_id and so on...
-func TestDeleteHorizontal(t *testing.T) {
-	recreateTestProductTables()
-
-	// Prepare a structure
-	var tp *TestProduct
-	for i := 1; i < 5; i++ {
-		tp = &TestProduct{
-			Name: fmt.Sprintf("Name%d", i),
-		}
-		testController.Save(tp)
-		testController.Save(&TestProductPrice{
-			Price:         44,
-			TestProductID: tp.ID,
-		})
-		testController.Save(&TestProductInfo{
-			Info:          fmt.Sprintf("Info%d", i),
-			TestProductID: tp.ID,
-		})
-	}
-
-	err := testController.DeleteHorizontal(tp, &TestProductPrice{}, &TestProductInfo{})
-	if err != nil {
-		t.Fatalf("DeleteHorizontal failed to delete records: %s", err.Op)
-	}
-
-	// TODO: Write actual tests
 }
