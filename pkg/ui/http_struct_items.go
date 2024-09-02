8254802b1b41fd28122d00d3dbc35d17e511e960
Mikolaj Gasior <mg@forthcoming.systems>
2024-09-02 09:56:14 +0200

    Make deletion of checked items work

--- pkg/ui/http_struct_items.go
+++ pkg/ui/http_struct_items.go
@@ -3,18 +3,22 @@ package ui
 import (
 	"fmt"
 	"net/http"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 
 func (c *Controller) tryStructItems(w http.ResponseWriter, r *http.Request, uri string) bool {
 	structName, id := c.getStructAndIDFromURI("x/struct_items/", c.getRealURI(uri, r.RequestURI))
 
-	if structName == "" {
+	if structName == "" || id != "" {
 		return false
 	}
 
 	// Check if struct exists
-	_, ok := c.uriStructNameFunc[uri][structName]
+	newObjFunc, ok := c.uriStructNameFunc[uri][structName]
 	if !ok {
 		w.WriteHeader(http.StatusBadRequest)
 		return true
@@ -24,17 +28,46 @@ func (c *Controller) tryStructItems(w http.ResponseWriter, r *http.Request, uri 
 		return false
 	}
 
-	if r.Method == http.MethodDelete && id == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return true
-	}
-
 	_ = c.uriStructNameFunc[uri][structName]()
 
 	// Handle delete here
 	if r.Method == http.MethodDelete {
 		// Read ids from the param 'ids'
+		ids := r.URL.Query().Get("ids")
+		match, err := regexp.MatchString(`^[0-9]+(,[0-9]+)*$`, ids)
+		if err != nil || !match {
+			w.WriteHeader(http.StatusBadRequest)
+			return true
+		}
+
 		// Run delete for multiple rows
+		// TODO: Replace it with a single delete happening on many IDs
+		idsList := strings.Split(ids, ",")
+		errInLoop := false
+		for _, id := range idsList {
+			obj := newObjFunc()
+
+			val := reflect.ValueOf(obj).Elem()
+			valField := val.FieldByName("ID")
+			if !valField.CanSet() {
+				errInLoop = true
+				continue
+			}
+			i, _ := strconv.ParseInt(id, 10, 64)
+			valField.SetInt(i)
+
+			err2 := c.struct2db.Delete(obj)
+			if err2 != nil {
+				errInLoop = true
+				continue
+			}
+		}
+
+		if errInLoop {
+			c.renderMsg(w, r, MsgFailure, fmt.Sprintf("Problem with removing %s items.", structName))
+			return true
+		}
+
 		c.renderMsg(w, r, MsgSuccess, fmt.Sprintf("%s items have been successfully deleted.", structName))
 		return true
 	}
