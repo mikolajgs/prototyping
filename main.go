c3769264956f09bd1c4ef895071ebbc283711f22
Mikolaj Gasior <m@gasior.dev>
2024-11-29 22:53:23 +0100

    WIP

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 	stdb "github.com/mikolajgs/prototyping/pkg/struct-db-postgres"
 	sqldb "github.com/mikolajgs/prototyping/pkg/struct-sql-postgres"
 	"github.com/mikolajgs/prototyping/pkg/ui"
-	"github.com/mikolajgs/prototyping/pkg/umbrella"
 	umb "github.com/mikolajgs/prototyping/pkg/umbrella"
 
 	_ "github.com/lib/pq"
@@ -69,7 +68,7 @@ func (p *Prototype) Run() error {
 
 	p.apiCtl = *restapi.NewController(p.db, p.dbTablePrefix, nil)
 	p.uiCtl = *ui.NewController(p.db, p.dbTablePrefix)
-	p.umbrella = *umbrella.NewUmbrella(p.db, p.dbTablePrefix, umb.JWTConfig{
+	p.umbrella = *umb.NewUmbrella(p.db, p.dbTablePrefix, umb.JWTConfig{
 		Key: "protoSecretKey",
 		Issuer: "prototyping.gasior.dev",
 		ExpirationMinutes: 5,
@@ -97,7 +96,7 @@ func (p *Prototype) Run() error {
 				p.constructors...,
 			))
 
-			http.Handle(p.uriUmbrella, &p.umbrella.GetHTTPHandler(p.uriUmbrella))
+			http.Handle(p.uriUmbrella, &p.umb.GetHTTPHandler(p.uriUmbrella))
 
 			log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", p.port), nil))
 		}()
